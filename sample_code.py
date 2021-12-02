071d0db0b7fcfa523c171482a9da38ae1bb865ce
Maru006 <87828918+Maru006@users.noreply.github.com>
2021-12-02 12:58:43 +0000

    Update sample_code.py
    more efficient way to seperate groups: feedback-type and task condition as main effects. However, another function may be added later to deal with interaction effects to the data.

--- sample_code.py
+++ sample_code.py
@@ -1,121 +1,86 @@
 import pandas as pd
-
-pd.set_option('display.max_rows', 1000)
-pd.set_option('display.max_columns', 1000)
-pd.set_option('display.width', 1000)
+import logging
 import re
+import numpy as np
 import itertools
+from matplotlib import pyplot as plt
+import seaborn as sns
 
-# recorded data for each participant separated between sheets
-data = pd.read_excel('RawData.xlsx', None)
-sheets = list(data.keys())  # shows all sheet names.
-
-# isolating relevant sheets.
-ID = []
-for participant_sheet in sheets:
-    matches = re.findall(r'[fF][pP]\d+_[vV]isit_\d', participant_sheet)
-    for match in matches:
-        ID.append(match)
-# used to confirm isolated sheet names to only participants'.
-sheets = data.keys()
-
-# # all sheets naturally contain reaction times for all trials regardless of rule or feedback type.
-fb = []
-nfb = []
-# FB-type
-fb_personal = []
-fb_process = []
-# rule-type
-bivalent = []
-univalent = []                        # might include or not until i find a more efficient way
-
-# FB/NFB x rule-type
-fb_bivalent = []
-fb_univalent = []
-
-nfb_bivalent = []
-nfb_univalent = []
-
-# FB-type x rule-type
-bivalentProcess = []
-univalentProcess = []
-bivalentPersonal = []
-univalentPersonal = []
+# pd.set_option('display.max_rows', 1000)
+# pd.set_option('display.max_columns', 1000)
+# pd.set_option('display.width', 1000)
+logging.basicConfig(level=logging.DEBUG, # helps debug visualizations later in the app process.
+                    format='%(funcName)s :: %(message)s')
 
 
-def feedback(IDs):
-    # BEWARE: driving an 'else' statement may be misleading when detecting 'equality': as is required with regex related functions.
-    # considering the analytical approach here, an 'identity' specific script may be more time consuming, hence it is avoided entirely.
-    sample = pd.read_excel('RawData.xlsx', sheet_name=IDs)
-    sample = sample.iloc[2:102, :]
-    univalent_regressor = sample[sample['Unnamed: 24'] == ' ']
-    bivalent_regressor = sample[sample['Unnamed: 24'] != ' ']
+class manipulate:
 
-    for column in list(sample.columns):
-        try:
-            feedback_type1 = re.findall(r'[pP]rocess', column)
-            feedback_type2 = re.findall(r'[pP]ersonal', column)
-            for t in feedback_type1 or feedback_type2:
-                if t == 'process':
-                    # print(IDs, '', column)
-                    fb.append({IDs: sample['Unnamed: 10']})
-                    fb_process.append({IDs: sample['Unnamed: 10']})
+    def __init__(self, url, sheet):
+        self.url = url
+        self.sheet = sheet
 
-                    bivalent.append({IDs: bivalent_regressor['Unnamed: 10']})
-                    fb_bivalent.append({IDs: bivalent_regressor['Unnamed: 10']})
-                    bivalentProcess.append({IDs: bivalent_regressor['Unnamed: 10']})
+    def data(self):
+        sheets = pd.read_excel(self.url, sheet_name=self.sheet)
+        return sheets
 
-                    univalent.append({IDs: univalent_regressor['Unnamed: 10']})
-                    fb_univalent.append({IDs: univalent_regressor['Unnamed: 10']})
-                    univalentPersonal.append({IDs: univalent_regressor['Unnamed: 10']})
-
-                if t == 'personal':
-                    # print(IDs, '', column)
-                    fb.append({IDs: sample['Unnamed: 10']})
-                    fb_personal.append({IDs: sample['Unnamed: 10']})
-
-                    bivalent.append({IDs: bivalent_regressor['Unnamed: 10']})
-                    fb_bivalent.append({IDs: bivalent_regressor['Unnamed: 10']})
-                    bivalentPersonal.append({IDs: bivalent_regressor['Unnamed: 10']})
-
-                    univalent.append({IDs: univalent_regressor['Unnamed: 10']})
-                    fb_univalent.append({IDs: univalent_regressor['Unnamed: 10']})
-                    univalentPersonal.append({IDs: univalent_regressor['Unnamed: 10']})
-
-            if None:
-                print(f"{IDs}'s sheet did not specify whether it had either the feedback condition compiled in regex. \n\t And so, {IDs} was skipped in the process")
+    def process_type(self):  # condition = feedback type.
+        sheets = pd.read_excel(self.url, sheet_name=self.sheet)
+        sheets.columns = sheets.columns.str.replace('Personal feedback', 'removed')
+        for column in sheets.columns:
+            try:
+                task_feedback = re.findall(r'[Pp]rocess', column)
+                trait_feedback = re.findall(r'[Pp]ersonal', column)
+                for match in task_feedback or trait_feedback:
+                    logging.info(f'In {self.sheet} sheet, {match} was found')
+                    if match == 'process':
+                        return 'process'
+                    if match == 'personal':
+                        return 'personal'
+                    if not task_feedback or trait_feedback:
+                        continue
+            except TypeError:  # instances columns contain an int
                 continue
-        except TypeError:
-            # should you encounter this error: This statement only capture raw strings based on the expression statement compiled at re.findall(r'') or specified by your own formats.
-            print(f"{IDs}'s did not specify whether it had either the feedback condition compiled in regex format.\n\t this may incur further error... you may wish to 'break' or 'continue'.")
-            user_input = input("type 'break' or 'continue': ")
-            if user_input == 'break':
-                break
-            if user_input == 'continue':
-                continue
-            if user_input != 'break' or 'continue':
-                return user_input
+
+    def process_variables(self, argument): # condition = task conditions.
+        data = pd.read_excel(self.url, sheet_name=self.sheet)
+        data = pd.concat([data.iloc[2:102], data.iloc[107:157]]).reset_index()
+        fb = data.loc[0:99, :].reset_index()
+        nfb = data.loc[100:155, :].reset_index()
+
+        variables = {'fb': data.loc[0:99, :].reset_index(),
+                     'nfb': data.loc[100:155, :].reset_index(),
+
+                     'bi': data.loc[np.where(data['Unnamed: 24'] != ' ')],
+                     'uni': data.loc[np.where(data['Unnamed: 25'] != ' ')],
+
+                     'fb_bi': fb.loc[np.where(fb['Unnamed: 24'] != ' ')],
+                     'fb_uni': fb.loc[np.where(fb['Unnamed: 25'] != ' ')],
+
+                     'nfb_bi': nfb.loc[np.where(nfb['Unnamed: 24'] != ' ')],
+                     'nfb_uni': nfb.loc[np.where(nfb['Unnamed: 25'] != ' ')],
+                     }
+        if argument in variables:
+            return variables[argument]
+        else:
+            logging.warning(f'{argument} not found in specified variables')
 
 
-def no_feedback(IDs):
-    sample = pd.read_excel('RawData.xlsx', sheet_name=IDs)
-    sample = sample.iloc[107:157, :]
-    nfb.append({IDs: sample['Unnamed: 10']})
+def main():
+    # isolating relevant sheets from the excel database.
+    ID = []
+    sheets = pd.read_excel('RawData.xlsx', None)
+    sheet = list(sheets.keys())
+    for participant_sheet in sheet:
+        matches = re.findall(r'[fF][pP]\d+_[vV]isit_\d', participant_sheet)
+        for match in matches:
+            ID.append(match)
+
+    # for i in ID:
+    #     data = manipulate('C:\\Users\\Maru\\PycharmProjects\\Dissertation\\RawData.xlsx', i)
+
+    data = manipulate('C:\\Users\\Maru\\PycharmProjects\\Dissertation\\RawData.xlsx', ID[1])
+    print(data.process_variables('notdict'))
 
 
-def rule_type(IDs):
-    sample = pd.read_excel('RawData.xlsx', sheet_name=IDs)
-    feedback_sample = sample.iloc[2:102, :]
-    no_feedback_sample = sample.iloc[107:157, :]
-
-    bivalent_cue = sample['Unnamed: 24'].index
-    univalent_cue = sample['Unnamed: 25'].index
-
-
-def terminal():
-    for i in ID:
-        feedback(i)
-        # no_feedback(i)
-
-
-terminal()
+if __name__ == '__main__':
+    main()
